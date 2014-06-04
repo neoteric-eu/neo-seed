4ec644b0434677a665e145be5a8771faab4c6600
Budmore <jmach@neoteric.eu>
2014-06-04 13:53:46 +0200

    documentTemplate - directives (old way)

--- app/modules/document/controllers/CreateDocumentController.js
+++ app/modules/document/controllers/CreateDocumentController.js
@@ -6,50 +6,193 @@
 		var CreateDocumentController = function ($scope, $routeParams) {
 			/* jshint  quotmark: false */
 			var mockedTemplate = {
-				"id": "53873466e4b05d88ac708a9e",
-				"name": "Dane kontaktowe",
-				"description": "a",
-				"timestamp": "2014-05-29T15:21:42.085+0200",
+				"id": "538868e2e4b09ae0edad108b",
+				"name": "Dane osobowe",
+				"description": "Lorem",
+				"timestamp": "2014-05-30T13:17:54.299+0200",
 				"metaFields": [
 				{
-					"id": "53873466e4b05d88ac708a9b",
+					"id": "538868e2e4b09ae0edad1085",
+					"fieldName": "Osoba kontaktowa",
+					"fieldDescription": "Lorem ipsum dolor sit amet senin! O to sło",
+					"fieldTypeId": "5379cc6c94c980bca9923d50",
+					"fieldTypeName": "TEXTFIELD",
+					"fieldTypeLabel": "Textfield",
+					"class": "PRIMITIVE",
+					"templateField": true,
+					"options": [],
+					"validationPattern": "^[0-9]+$",
+					"required": true,
+					"composite": [],
+					"value": null
+				},
+
+				{
+					"id": "538868e2e4b09ae0edad1085",
+					"fieldName": "Osoba kontaktowa",
+					"fieldDescription": "Hello world",
+					"fieldTypeId": "5379cc6c94c980bca9923d50",
+					"fieldTypeName": "TEXTFIELD",
+					"fieldTypeLabel": "Textfield",
+					"class": "PRIMITIVE",
+					"templateField": true,
+					"options": [],
+					"validationPattern": "^[0-9]+$",
+					"required": false,
+					"composite": [],
+					"value": null
+				},
+				{
+					"id": "538868e2e4b09ae0edad1085",
 					"fieldName": "Osoba kontaktowa",
 					"fieldDescription": "",
 					"fieldTypeId": "5379cc6c94c980bca9923d50",
-					"fieldTypeName": "Textfield",
+					"fieldTypeName": "TEXTFIELD",
+					"fieldTypeLabel": "Textfield",
 					"class": "PRIMITIVE",
+					"templateField": true,
 					"options": [],
-					"validationPattern": null,
-					"required": false,
+					"validationPattern": "",
+					"required": true,
 					"composite": [],
 					"value": null
 				},
 				{
-					"id": "53873466e4b05d88ac708a9c",
-					"fieldName": "email",
-					"fieldDescription": "",
-					"fieldTypeId": "5379cc6c94c980bca9923d51",
-					"fieldTypeName": "Email",
-					"class": "PRIMITIVE",
-					"options": [],
-					"validationPattern": null,
-					"required": false,
-					"composite": [],
-					"value": null
-				},
-				{
-					"id": "53873466e4b05d88ac708a9d",
-					"fieldName": "nr tel",
-					"fieldDescription": "",
+					"id": "538868e2e4b09ae0edad1085",
+					"fieldName": "Osoba kontaktowa",
+					"fieldDescription": "Hello world",
 					"fieldTypeId": "5379cc6c94c980bca9923d50",
-					"fieldTypeName": "Textfield",
+					"fieldTypeName": "TEXTFIELD",
+					"fieldTypeLabel": "Textfield",
 					"class": "PRIMITIVE",
+					"templateField": true,
+					"options": [],
+					"validationPattern": "",
+					"required": false,
+					"composite": [],
+					"value": null
+				},
+				{
+					"id": "538868e2e4b09ae0edad1086",
+					"fieldName": "Email",
+					"fieldDescription": "Lorem ip[sum dolor sit amet",
+					"fieldTypeId": "5379cc6c94c980bca9923d51",
+					"fieldTypeName": "EMAIL",
+					"fieldTypeLabel": "Email",
+					"class": "PRIMITIVE",
+					"templateField": true,
+					"options": [],
+					"validationPattern": "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$",
+					"required": true,
+					"composite": [],
+					"value": null
+				},
+				{
+					"id": "538868e2e4b09ae0edad1087",
+					"fieldName": "Płeć",
+					"fieldDescription": "",
+					"fieldTypeId": "5379cc6c94c980bca9923d52",
+					"fieldTypeName": "RADIO",
+					"fieldTypeLabel": "Radio button",
+					"class": "PRIMITIVE",
+					"templateField": true,
 					"options": [],
 					"validationPattern": null,
 					"required": false,
 					"composite": [],
 					"value": null
-				}
+				},
+				{
+					"id": "538868e2e4b09ae0edad1088",
+					"fieldName": "dropdown",
+					"fieldDescription": "",
+					"fieldTypeId": "5379cc6c94c980bca9923d53",
+					"fieldTypeName": "DROPDOWN",
+					"fieldTypeLabel": "Dropdown list",
+					"class": "PRIMITIVE",
+					"templateField": true,
+					"options": [],
+					"validationPattern": null,
+					"required": false,
+					"composite": [],
+					"value": null
+				},
+				{
+					"id": "538868e2e4b09ae0edad1089",
+					"fieldName": "Date",
+					"fieldDescription": "",
+					"fieldTypeId": "5379cc6c94c980bca9923d54",
+					"fieldTypeName": "DATE",
+					"fieldTypeLabel": "Date",
+					"class": "PRIMITIVE",
+					"templateField": true,
+					"options": [],
+					"validationPattern": null,
+					"required": true,
+					"composite": [],
+					"value": null
+				},
+				{
+					"id": "538868e2e4b09ae0edad108a",
+					"fieldName": "Regulamin",
+					"fieldDescription": "Czy oddasz nerke?",
+					"fieldTypeId": "5379cc6c94c980bca9923d55",
+					"fieldTypeName": "CHECKBOX",
+					"fieldTypeLabel": "Checkbox",
+					"class": "PRIMITIVE",
+					"templateField": true,
+					"options": [],
+					"validationPattern": null,
+					"required": true,
+					"composite": [],
+					"value": null
+				},
+
+
+
+                {
+                    "id": "538873afe4b09ae0edad108d",
+                    "fieldName": "Miejsce spotkania",
+                    "fieldDescription": "Hola hola żołnierzyku",
+                    "fieldTypeId": "5379cc6c94c980bca9923d52",
+                    "fieldTypeName": "RADIO",
+                    "fieldTypeLabel": "Radio button",
+                    "class": "PRIMITIVE",
+                    "templateField": true,
+                    "options": [
+                        "1",
+                        "2",
+                        "3",
+                        "4"
+                    ],
+                    "validationPattern": null,
+                    "required": true,
+                    "composite": [],
+                    "value": "3"
+                },
+                {
+                    "id": "538873afe4b09ae0edad108e",
+                    "fieldName": "123123",
+                    "fieldDescription": "",
+                    "fieldTypeId": "5379cc6c94c980bca9923d53",
+                    "fieldTypeName": "DROPDOWN",
+                    "fieldTypeLabel": "Dropdown list",
+                    "class": "PRIMITIVE",
+                    "templateField": true,
+                    "options": [
+                        "1",
+                        "2",
+                        "3",
+                        "4",
+                        "5",
+                        "6"
+                    ],
+                    "validationPattern": null,
+                    "required": true,
+                    "composite": [],
+                    "value": "3"
+                }
+
 				],
 				"userId": "5357699c9d33da5ee72b45ce",
 				"customerId": "5351090b8fe7f4e7b99d6e67",
@@ -58,12 +201,11 @@
 					"version": 1,
 					"previousVersion": null,
 					"author": "admin@neoteric.eu",
-					"timestamp": "2014-05-29T15:21:42.085+0200"
+					"timestamp": "2014-05-30T13:17:54.299+0200"
 				}
 				],
 				"version": 1
 			};
-
 
 			$scope.initDocument = function() {
 
