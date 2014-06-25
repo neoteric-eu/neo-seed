46f35faa1d09289fa437f4d403de661fa07abb4a
Budmore <jmach@neoteric.eu>
2014-06-25 14:14:31 +0200

    remove lang en from global_settings

--- config/global_settings.js
+++ config/global_settings.js
@@ -15,8 +15,7 @@
 				// Language code for this installation. All choices can be found here:
 				// http://www.i18nguy.com/unicode/language-identifiers.html
 				LANGUAGES: [
-					{code: 'pl', name:'Polish'},
-					{code: 'en', name:'English'}
+					{code: 'pl', name:'Polish'}
 				]
 			},
 			get: function(element) {
