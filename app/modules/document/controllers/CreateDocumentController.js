976cc049b7931ef4bbac8f557c8a2ee788036b41
Budmore <jmach@neoteric.eu>
2014-06-30 13:54:05 +0200

    preview document

--- app/modules/document/controllers/CreateDocumentController.js
+++ app/modules/document/controllers/CreateDocumentController.js
@@ -8,18 +8,146 @@
 			documentService, documentModulePath) {
 
 			// Setup environment
-			$scope.dateFormat = 'dd-MM-yyyy';
+			$scope.dateFormat = 'dd-MM-yyyy, HH:mm';
 			$scope.editMode = false;
 			$scope.readyToShow = false;
 			$scope.templateCreatorMode = false;
 
-			$scope.isIconEditable = false;
+			// TODO: remove HARDCODE
+			/* jshint unused:false */
+			var mocked = {
+				'id': 'MOCKED_DOC_ID_2',
+				'templateId': null,
+				'templateName': null,
+				'templateVersion': null,
+				'latestTemplateVersion': null,
+				'userId': '53576d399d33da5ee72b45d1',
+				'email': 'test@neoteric.eu',
+				'customerId': '53576d7c9d33da5ee72b45d2',
+				'name': 'MOCKED_DOC_ID_2',
+				'description': 'lorem ipsum',
+				'timestamp': '2014-06-17T11:17:30.997+0200',
+				'versions': [
+				{
+					'version': 1,
+					'previousVersion': null,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T13:32:38.049+0200'
+				},
+				{
+					'version': 2,
+					'previousVersion': 1,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T13:33:29.167+0200'
+				},
+				{
+					'version': 3,
+					'previousVersion': 1,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T15:48:33.920+0200'
+				},
+				{
+					'version': 4,
+					'previousVersion': 2,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T15:54:39.953+0200'
+				},
+				{
+					'version': 5,
+					'previousVersion': 1,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T16:04:37.524+0200'
+				},
+				{
+					'version': 6,
+					'previousVersion': 5,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T16:04:49.443+0200'
+				},
+				{
+					'version': 7,
+					'previousVersion': 1,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T16:38:00.900+0200'
+				},
+				{
+					'version': 8,
+					'previousVersion': 2,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T16:38:13.557+0200'
+				},
+				{
+					'version': 9,
+					'previousVersion': 3,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T16:47:50.093+0200'
+				},
+				{
+					'version': 10,
+					'previousVersion': 4,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T16:47:55.126+0200'
+				},
+				{
+					'version': 11,
+					'previousVersion': 9,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-16T17:19:42.165+0200'
+				},
+				{
+					'version': 12,
+					'previousVersion': 6,
+					'author': 'test@neoteric.eu',
+					'timestamp': '2014-06-17T11:17:30.997+0200'
+				}
+				],
+				'metaFields': [
+				{
+					'id': '539ed5d6e4b04c1fd3d6e410',
+					'fieldName': 'Tekst',
+					'fieldDescription': '',
+					'fieldTypeId': '5379cc6c94c980bca9923d50',
+					'fieldTypeName': 'TEXTFIELD',
+					'fieldTypeLabel': 'Textfield',
+					'class': 'PRIMITIVE',
+					'templateField': false,
+					'options': [],
+					'validationPattern': null,
+					'required': false,
+					'composite': [],
+					'value': null
+				},
+				{
+					'id': '539ed5d6e4b04c1fd3d6e410',
+					'fieldName': 'attachment',
+					'fieldDescription': '',
+					'fieldTypeId': '5379cc6c94c980bca9923d50',
+					'fieldTypeName': 'ATTACHMENT',
+					'fieldTypeLabel': 'Textfield',
+					'class': 'PRIMITIVE',
+					'templateField': false,
+					'options': [],
+					'validationPattern': null,
+					'required': false,
+					'composite': [],
+					'value': 'MOCKED_DOC_ID_22'
+				}
+				],
+				'version': 12,
+				'privileges': [
+				'ALL'
+				],
+				'sharingInfo': null,
+				'icon': 'fa-file-text-o'
+			};
+
+
 			/**
 			 *	@name init
 			 *
 			 *	@description
 			 *
-			 *	The are 4x "types" of the document:
+			 *	The are 4x 'types' of the document:
 			 *	- create new by template
 			 *	- create new by templateCreator
 			 *	- edit document by id
@@ -54,19 +182,21 @@
 			$scope.createNewByTemplate = function(templateId) {
 				$scope.document = {};
 				system.showLoader();
-				documentTemplateService.getTemplateById(templateId).then(function(data) {
-					$scope.documentTemplate = data;
-					$scope.document.icon = data.icon;
-					$scope.document.name = data.name;
-					$scope.document.description = data.description;
+				documentTemplateService.getTemplateById(templateId).then(
+					function(data) {
+						$scope.documentTemplate = data;
+						$scope.document.icon = data.icon;
+						$scope.document.name = data.name;
+						$scope.document.description = data.description;
 
-					$scope.document.templateId = data.id;
-					$scope.document.metaFields = data.metaFields;
+						$scope.document.templateId = data.id;
+						$scope.document.metaFields = data.metaFields;
 
-				}, function() {
-					appMessages.error(locale.getT('template_was_not_loaded_please_refresh_browser'));
+					}, function() {
+						appMessages.error(locale.getT('template_was_not_loaded_please_refresh_browser'));
 
-				}).finally(function() {
+					}
+				).finally(function() {
 					$scope.readyToShow = true;
 					system.hideLoader();
 				});
@@ -81,9 +211,13 @@
 			 */
 			$scope.editDocumentById = function(documentId) {
 				system.showLoader();
-				documentService.getDocumentById(documentId).then(function() {
+				documentService.getDocumentById(documentId).then(function(data) {
 					$scope.editMode = true;
-					$scope.document = documentService.activeDocument.getModel();
+					documentService.activeDocument.setModel(data);
+					documentService.previewDocument.setModel(data);
+					// $scope.document = documentService.activeDocument.getModel();
+					//TODO: Mocked data
+					$scope.document = mocked;
 
 				}, function() {
 					appMessages.error(locale.getT('Operation_failed'));
@@ -93,6 +227,30 @@
 					system.hideLoader();
 				});
 			};
+
+
+			/**
+			 *	@name getDocumentById
+			 *
+			 *	@param {String} documentId
+			 *	@description get document by id
+			 *	@return {object}
+			 */
+/*			$scope.getDocumentById = function(documentId) {
+				return documentService.getDocumentById(documentId).then(function(data) {
+					console.log('aaa', data);
+					$scope.document = mocked;
+
+				}, function() {
+					appMessages.error(locale.getT('Operation_failed'));
+
+				}).finally(function() {
+					$scope.readyToShow = true;
+					system.hideLoader();
+				});
+			};*/
+
+
 
 			/**
 			 *	@name createNewByTemplateCreator
@@ -186,33 +344,36 @@
 			};
 
 
-			$scope.docPreviewModal = function(previewDocument, previewVersion) {
-				console.log(previewDocument, '!!!!!!!!', previewVersion);
+			$scope.docPreviewModal = function(activeDocument, previewVersion) {
+
 				var modalInstance = $modal.open({
 					templateUrl: documentModulePath + 'views/modals/docPreview.html',
 					controller: 'PreviewModalController',
 					windowClass: 'docPreview',
 					resolve: {
-						previewDocument: function(){
-							return previewDocument;
+						activeDocument: function(){
+							return activeDocument;
 						},
 						previewVersion: function(){
 							return previewVersion;
 						}
 					}
 				});
-				modalInstance.result.then(function (previewDocument) {
-					$scope.document = previewDocument;
-				});
 
+				modalInstance.result.then(function (previewDocument) {
+					$scope.document = documentService.activeDocument.getModel();
+				});
 			};
 
 			$scope.restoreDocumentVersion = function(previewDocument, previewVersion) {
-				documentService.restoreDocumentVersion(previewDocument.id, previewVersion.version).then(function() {
-					$scope.previewDocument = documentService.previewDocument.getModel();
-					$scope.document = angular.copy($scope.previewDocument);
-				}, function() { // reason
-					// $exceptionHandler(reason);
+				system.showLoader();
+				documentService.restoreDocumentVersion(previewDocument.id, previewVersion.version).then(
+					function(data) {
+						documentService.activeDocument.setModel(data);
+						$scope.document = documentService.activeDocument.getModel();
+					}
+				).finally(function() {
+					system.hideLoader();
 				});
 			};
 
--- app/modules/templateCore/templateCoreUrls.js
+++ app/modules/templateCore/templateCoreUrls.js
@@ -3,10 +3,10 @@
 	define([] , function() {
 		var templateCoreUrls = function($routeProvider, PATH_TEMPLATECORE) {
 			$routeProvider
-			.when('/documents', {templateUrl: PATH_TEMPLATECORE + 'views/example.html', access:'SM_USER_GET' })
+			.when('/start', {templateUrl: PATH_TEMPLATECORE + 'views/example.html', access:'SM_USER_GET' })
 
 			.when('/login', {templateUrl: PATH_TEMPLATECORE + 'views/login-page.html', access:'ONLY_NOT_LOGGED'})
-			.otherwise({redirectTo: '/documents'});
+			.otherwise({redirectTo: '/start'});
 		};
 		return ['$routeProvider', 'PATH_TEMPLATECORE', templateCoreUrls];
 	});
