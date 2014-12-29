f7a17d4b31deb086ae6d236dd89208241b234996
Błażej Krysiak <bkrysiak@neoteric.eu>
2014-12-29 13:00:33 +0100

    Added extended logging.

--- config/global_settings.js
+++ config/global_settings.js
@@ -50,35 +50,7 @@
 				APP_NAME: 'Neoteric',
 
 				MOD_REWRITE: '@@MOD_REWRITE',
-				COOKIE_DOMAIN: '@@COOKIE_DOMAIN',
-
-				FEATURES: [
-					'ONLY_NOT_LOGGED',
-
-					// Application
-					'SM_APPLICATION_GET',
-
-					// Bundle
-					'SM_BUNDLE_GET',
-					'SM_BUNDLE_BUY',
-
-					// Customer
-					'SM_CUSTOMER_GET',
-					'SM_CUSTOMER_CREATE',
-					'SM_CUSTOMER_UPDATE',
-					'SM_CUSTOMER_DELETE',
-
-					// Role
-					'SM_ROLE_GET',
-					'SM_ROLE_CREATE',
-					'SM_ROLE_UPDATE',
-					'SM_ROLE_DELETE',
-
-					// User
-					'SM_USER_GET',
-					'SM_USER_UPDATE'
-				]
-
+				COOKIE_DOMAIN: '@@COOKIE_DOMAIN'
 			},
 			get: function(element) {
 				try {
