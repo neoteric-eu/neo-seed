1fc6d650c88939467e2028642c73f60e44eceb2d
Błażej Krysiak <bkrysiak@neoteric.eu>
2015-07-30 13:08:06 +0200

    Removed demo.

--- src/apps/demo/components/projects/Project.js
+++
@@ -1,9 +0,0 @@
-define(['apps/demo/module'], function(app){
-    "use strict";
-
-    return app.factory('Project', function($http){
-        return {
-            list: $http.get('apps/demo/api/projects.json')
-        }
-    })
-})
