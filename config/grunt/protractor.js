b18ded06dd17b47094f1397bf485d48a3b810c7d
Błażej Krysiak <bkrysiak@neoteric.eu>
2015-08-04 18:50:28 +0200

    Modularized seed

--- config/grunt/protractor.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	options: {
-		configFile: './config/protractor/protractor.conf.js',
-		keepAlive: false,
-		noColor: true
-	},
-	singlerun: {}
-};
--- src/apps/dashboard/_locale/translation.js
+++
@@ -1,6 +0,0 @@
-define(['angular', 'seed'], function (angular) {
-	angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
-		/* jshint -W100 */
-		/* jshint +W100 */
-	}]);
-});
