c0f39cb63b146a18a3e2224eb5114d6fc8fb1218
Błażej Krysiak <bkrysiak@neoteric.eu>
2015-11-17 16:38:25 +0100

    feat(neoPageTitle): #3304 - Add missing neoPageTitle directive
    BREAKING CHANGE:
    Now application is bootstrapped on <html> tag instead of <body>
    
    Closes #3304

--- components/pageTitle/neoPageTitle.js
+++ components/pageTitle/neoPageTitle.js
@@ -1,23 +1,45 @@
 define(['seed/components/module'], function (module) {
 	'use strict';
 
+	/**
+	 * Displays customized page title based on state of application.
+	 * @class neoPageTitle
+	 * @memberOf seed.components
+	 *
+	 * @example
+	 * <title neo-page-title></title>
+	 *
+	 * Along with module definition:
+	 *
+	 * module.config(function ($stateProvider) {
+	 * 	$stateProvider
+	 * 	.state('app.dashboard', {
+	 * 		[...]
+	 * 		data: {
+	 * 		title: 'Dashboard'
+	 * 		}
+	 * 		});
+	 * });
+	 *
+	 * @param $log {Object} Logging service
+	 * @param appConf {Object} Application configuration
+	 * @returns {{restrict: string, controller: Function}}
+	 */
 	function neoPageTitle($log, appConf) {
 		return {
 			restrict: 'A',
-			controller: function ($scope, $element) {
+			controller: function ($scope, $element, gettextCatalog) {
 
 				$log = $log.getInstance('seed.components.neoPageTitle');
 				$log.debug('Initiated directive');
 
 				$scope.$root.$on('$stateChangeStart', function (event, toState) {
+
 					var title = appConf.generalSettings.name;
 					if (_.has(toState.data, 'title')) {
-						title += (' | ' + toState.data.title);
+						title += (' | ' + gettextCatalog.getString(toState.data.title));
 					}
-
-					//$scope.$applyAsync(function () {
 					$element.text(title);
-					//});
 
 					$log.debug('Updated page title attribute');
 				});
