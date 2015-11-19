ae2a62b48e6c31bd5900f290ca44bfcf527afc2d
Błażej Krysiak <bkrysiak@neoteric.eu>
2015-11-19 15:34:57 +0100

    style(NeoStyleAPI): space to tab indentation

--- helpers/restmod/styles/NeoStyleAPI.js
+++ helpers/restmod/styles/NeoStyleAPI.js
@@ -1,30 +1,30 @@
 define(['seed/helpers/module'], function (app) {
-	'use strict';
+  'use strict';
 
-	/**
-	 * JSON:API standard data interpreter settings
-	 * @interface
-	 * @memberOf seed.helpers
-	 *
-	 * @param restmod {Object} Restmod service provider
-	 * @param appConf {Object} Application configuration
-	 * @return {void|*|{$isAbstract, $$chain}|Function|Object}
-	 */
-	function NeoStyleAPI(restmod, appConf) {
-		return restmod.mixin(
-			'DefaultPacker',
+  /**
+   * JSON:API standard data interpreter settings
+   * @interface
+   * @memberOf seed.helpers
+   *
+   * @param restmod {Object} Restmod service provider
+   * @param appConf {Object} Application configuration
+   * @return {void|*|{$isAbstract, $$chain}|Function|Object}
+   */
+  function NeoStyleAPI(restmod, appConf) {
+    return restmod.mixin(
+      'DefaultPacker',
       'restmod.FindMany',
-			'restmod.Preload',
-			'BaseModel', {
-				$config: {
-					style: 'NeoStyleAPI',
-					urlPrefix: appConf.environmentSettings.apiUrl,
-					primaryKey: 'id',
-					jsonRoot: 'data',
-					jsonMeta: 'meta'
-				}
-			});
-	}
+      'restmod.Preload',
+      'BaseModel', {
+        $config: {
+          style: 'NeoStyleAPI',
+          urlPrefix: appConf.environmentSettings.apiUrl,
+          primaryKey: 'id',
+          jsonRoot: 'data',
+          jsonMeta: 'meta'
+        }
+      });
+  }
 
-	app.factory('NeoStyleAPI', NeoStyleAPI);
+  app.factory('NeoStyleAPI', NeoStyleAPI);
 });
