8de908725f0e318a166548de0126abba520b5b25
Jakub Matyka <kmatyka@ntrc.eu>
2016-03-22 13:02:02 +0100

    test: authProfileSelectForm small fixes in authProfileSelectForm

--- src/seed/auth/test/login/authProfileSelectForm.spec.js
+++ src/seed/auth/test/login/authProfileSelectForm.spec.js
@@ -9,9 +9,12 @@ define([
 			describe('module: login', function () {
 				describe('directive: authProfileSelectForm', function () {
 
-					var $q, scope, $state, $compile, element, $timeout, UserAPI, LanguageAPI, Permission;
+					var $q, scope, $state, $compile, $stateProvider, appConf, $rootScope, element, $timeout, UserAPI, LanguageAPI, Permission, neoSession;
 
 					beforeEach(function () {
+						module('ui.router', function ($injector) {
+							$stateProvider = $injector.get('$stateProvider')
+						});
 						inject(function ($injector) {
 							$q = $injector.get('$q');
 							$compile = $injector.get('$compile');
@@ -20,6 +23,9 @@ define([
 							LanguageAPI = $injector.get('LanguageAPI');
 							$state = $injector.get('$state');
 							Permission = $injector.get('Permission');
+							neoSession = $injector.get('neoSession');
+							$rootScope = $injector.get('$rootScope');
+							appConf = $injector.get('appConf')
 						});
 					});
 
@@ -30,13 +36,27 @@ define([
 						element = $compile('<auth-profile-select-form></auth-profile-select-form>')(scope);
 					});
 
+					beforeEach(function () {
+						inject(function () {
+							spyOn(LanguageAPI, 'getLanguage').and.callFake(function () {
+								return {localePOSIX: 'en_GB'};
+							});
+							scope.$root.user = UserAPI.build({
+								login: 'exampleUser',
+								password: 'examplePassword'
+							});
+							spyOn(Permission, 'authorize').and.callFake(function () {
+								return $q.resolve();
+							});
+							spyOn(neoSession, 'setSession').and.callFake(function () {
+								return $q.resolve();
+							});
+						})
+					});
+
 
 					it('should go to auth.login state on init if user is not set', function () {
 						//GIVEN
-						spyOn(Permission, 'authorize').and.callFake(function () {
-							return $q.resolve();
-						});
-
 						//WHEN
 						scope.$digest();
 
@@ -48,13 +68,7 @@ define([
 						//GIVEN
 						var sut = 'exampleCustomer';
 
-						spyOn(LanguageAPI, 'getLanguage').and.callFake(function () {
-							return {localePOSIX: 'en_GB'};
-						});
-
-						scope.$root.user = UserAPI.build({
-							login: 'exampleUser',
-							password: 'examplePassword',
+						scope.$root.user.$extend({
 							customers: [sut]
 						});
 
@@ -67,90 +81,91 @@ define([
 					});
 
 					it('should on click set activeCustomer selected by user', function () {
+						//GIVEN
+						var sut = 'customer2';
+
+						scope.$root.user.$extend({
+							customers: ['customer1', sut]
+						});
+
+						//WHEN
+						scope.$digest();
+						var vm = element.controller('authProfileSelectForm');
+						vm.setCustomerActive(sut);
+
+						//THEN
+						expect(vm.activeCustomer).toEqual(sut);
 					});
 
-					it('should on click set activeCustomer selected by user', function () {
-					});
+					fit('should navigate by default to selection of profile when successfully logged in with user with multiple profiles', function () {
+						//GIVEN
+						spyOn(UserAPI, 'login').and.callFake(function () {
+							return $q.resolve(UserAPI.build({
+								customers: [
+									{
+										featureKeys: []
+									},
+									{
+										featureKeys: []
+									}
+								]
+							}));
+						});
 
+						$rootScope.$digest();
+						var vm = element.controller('authProfileSelectForm');
+						scope.$root.requestedState = {
+							toState: 'app.dashboard'
+						};
 
-					it('should navigate by default to selection of profile when successfully logged in with user with multiple profiles', function () {
-						// GIVEN
-						//spyOn(UserAPI, 'login').and.callFake(function () {
-						//	return $q.resolve(UserAPI.build({
-						//		customers: [
-						//			{
-						//				featureKeys: []
-						//			},
-						//			{
-						//				featureKeys: []
-						//			}
-						//		]
-						//	}));
-						//});
-						//
-						//var $state = $inject('$state');
-						//
-						//spyOn($state, 'go').and.callFake(function () {
-						//	return $q.resolve();
-						//});
-						//
-						//spyOn(neoSession, 'setSession').and.callFake(function () {
-						//	return $q.resolve();
-						//});
-						//
-						//var scope = $rootScope.$new();
-						//var element = $compile('<auth-login-form></auth-login-form>')(scope);
-						//$rootScope.$digest();
-						//
-						//var vm = element.controller('authLoginForm');
-						//vm.user.$extend({
-						//	login: 'exampleUser',
-						//	password: 'examplePassword'
-						//});
-						//
-						//// WHEN
-						//vm.login();
-						//$timeout.flush();
-						//
-						//// THEN
-						//expect(UserAPI.login).toHaveBeenCalled();
-						//expect($rootScope.user).toBeDefined();
-						//expect($state.go).toHaveBeenCalledWith('auth.profileSelect', {}, {notify: true, reload: false});
+						vm.activeCustomer = 'customer';
+
+						$stateProvider
+							.state('app', {
+								abstract: true
+							})
+							.state('app.dashboard', {
+								url: '/dashboard'
+							});
+
+						// WHEN
+						vm.login();
+						$timeout.flush();
+
+						// THEN
+						expect($state.current.name).toEqual('app.dashboard');
 					});
 
 					it('should redirect by default to defaultRedirectStateAfterLogin when successfully logged in with one profile', function () {
-						// GIVEN
-						//	spyOn(UserAPI, 'login').and.callFake(function () {
-						//		return $q.resolve(UserAPI.build());
-						//	});
-						//
-						//	spyOn(neoSession, 'setSession').and.callFake(function () {
-						//		return $q.resolve();
-						//	});
-						//
-						//	var appConf = $injector('appConf');
-						//
-						//	var scope = $rootScope.$new();
-						//	var element = $compile('<auth-login-form></auth-login-form>')(scope);
-						//	$rootScope.$digest();
-						//
-						//	var vm = element.controller('authLoginForm');
-						//	vm.user.$extend({
-						//		login: 'exampleUser',
-						//		password: 'examplePassword'
-						//	});
-						//
-						//	// WHEN
-						//	vm.login();
-						//	$rootScope.$digest();
-						//	$timeout.flush();
-						//
-						//	// THEN
-						//	expect(UserAPI.login).toHaveBeenCalled();
-						//	expect($rootScope.user).toBeDefined();
-						//	expect($state.current.name).toEqual(appConf.generalSettings.defaultRedirectStateAfterLogin);
-					});
+						//GIVEN
+						$stateProvider
+							.state('app', {
+								abstract: true
+							})
+							.state('app.dashboard', {
+								url: '/dashboard'
+							});
 
+						var sut = {
+							customerName: 'customer'
+						};
+
+						$rootScope.$digest();
+						var vm = element.controller('authProfileSelectForm');
+						vm.activeCustomer = sut;
+
+						scope.$root.user.$extend({
+							customers: [sut]
+						});
+
+						// WHEN
+						vm.login();
+						scope.$digest();
+						$timeout.flush();
+
+						// THEN
+						expect($state.current.name).toEqual(appConf.generalSettings.defaultRedirectStateAfterLogin);
+					});
 				});
 			});
 		});
