375c175ab7ed3d2d95d7a1b861c752e14833b488
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2016-03-23 13:50:35 +0100

    test(component): adding test for neoVersionTag

---
+++ src/seed/components/test/neoVersionTag.spec.js
@@ -0,0 +1,70 @@
+define([
+  'angular',
+  'angular-mocks',
+  'seed/components/_includes',
+  'seed/components/module',
+  'seed/components/versionTag/neoVersionTag.html'
+], function () {
+  'use strict';
+
+  describe('module: seed', function () {
+    describe('module: components', function () {
+      describe('directive: neoPageTitle', function () {
+
+        var $compile, $rootScope, $log, appConf;
+        var generalSettings = {
+          version: '1.0',
+          ciBuildNumber: '1234'
+        };
+
+        beforeEach(function () {
+          module(function ($provide) {
+            $provide.value('appConf');
+          });
+
+          module('seed.components');
+        });
+
+        beforeEach(function () {
+          // Inject service into module
+          inject(function ($injector) {
+            $compile = $injector.get('$compile');
+            $rootScope = $injector.get('$rootScope');
+            $log = $injector.get('$log');
+            appConf = $injector.get('appConf');
+          });
+        });
+
+        it('should display version tag with version and ci build version', function () {
+          // GIVEN
+          appConf.generalSettings = generalSettings;
+          var scope = $rootScope.$new();
+          var element = $compile('<neo-version-tag/>')(scope);
+
+          // WHEN
+          scope.$digest();
+
+          // THEN
+          expect(element.text().indexOf('ver: ' + generalSettings.version)).toBeGreaterThan(-1);
+          expect(element.find('span').text()).toBe('(#' + generalSettings.ciBuildNumber + ')');
+        });
+
+        it('should display only version tag', function () {
+          // GIVEN
+          appConf.generalSettings = {
+            version: generalSettings.version
+          };
+          var scope = $rootScope.$new();
+          var element = $compile('<neo-version-tag/>')(scope);
+
+          // WHEN
+          scope.$digest();
+
+          // THEN
+          expect(element.text().indexOf('ver: ' + generalSettings.version)).toBeGreaterThan(-1);
+          expect(element.find('span').length).toBe(0);
+        });
+      });
+    });
+  });
+});
