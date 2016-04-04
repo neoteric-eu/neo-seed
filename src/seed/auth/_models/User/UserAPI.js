8228f1a5b77836863814f1caaa5a2418e10313aa
Błażej Krysiak <bkrysiak@neoteric.eu>
2016-04-04 12:48:41 +0200

    wip: Reverted UserAPI changes on development

--- src/seed/auth/_models/User/UserAPI.js
+++ src/seed/auth/_models/User/UserAPI.js
@@ -1,145 +1,114 @@
 define(['seed/auth/module'], function (module) {
-		'use strict';
+	'use strict';
 
-		/**
-		 * Interface for REST communication with server
-		 * @constructor
-		 * @implements {seed.helpers.BaseAPI}
-		 * @memberOf seed.auth
-		 *
-		 * @param $log {Object} Logging service
-		 * @param $q
-		 * @param $http
-		 * @param BaseAPI {Function} Base interface for REST communication with server
-		 * @param User {Object} Model factory
-		 * @param appConf
-		 * @return {Function} Instantiated service
-		 */
-		var UserAPI = function ($log, $q, $http, BaseAPI, User, appConf) {
+	/**
+	 * Interface for REST communication with server
+	 * @constructor
+	 * @implements {seed.BaseAPI}
+	 * @memberOf seed.auth
+	 *
+	 * @param $q {Object} Angular promise implementation
+	 * @param $log {Object} Logging service
+	 * @param $cookies {Function} Cookie service
+	 * @param $rootScope {Object} Global scope provider
+	 * @param BaseAPI {Function} Base interface for REST communication with server
+	 * @param User {Object} Model factory
+	 * @return {Function} Instantiated service
+	 */
+	var UserAPI = function ($log, $cookies, $rootScope, BaseAPI, User, $q) {
 
-			$log = $log.getInstance('seed.auth.UserAPI');
-			$log.debug('Initiated service');
+		$log = $log.getInstance('seed.auth.UserAPI');
+		$log.debug('Initiated service');
 
-			var api = new BaseAPI(User);
+		var api = new BaseAPI(User);
 
-			api.login = login;
-			api.logout = logout;
-			api.register = register;
-			api.authInfo = authInfo;
-			api.resetPasswordInit = resetPasswordInit;
-			api.resetPasswordFinish = resetPasswordFinish;
-
-			function login(loginData) {
-				return $http.get({
-						url: api.model.$url() + '/login',
-						data: {
-							login: loginData.login,
-							password: loginData.password
-						}
-					})
-					.then(function (user) {
-						$log.debug('Successfully logged in user');
-
-						return User.$decode(user);
-					})
-					.catch(function (reason) {
-						$log.error('Could not login the user');
-
-						return $q.reject(reason);
-					});
-			}
-
-			function register(user) {
-				return $http.post({
-						url: appConf.environmentSettings.apiUrl + '/registration',
-						data: user
-					})
-					.then(function (user) {
-						$log.debug('Successfully registered new user');
-
-						return User.$decode(user);
-					})
-					.catch(function (reason) {
-						$log.error('Error registering new user');
-
-						return $q.reject(reason);
-					});
-			}
-
-			function authInfo() {
-				return $http.post({
-						url: this.$scope.$url() + '/authInfo',
-						cache: true,
-						data: {
-							token: $cookies.getObject('token')
-						}
-					})
-					.then(function (user) {
-						$log.debug('Successfully verified user session');
-
-						return User.$decode(user);
-					})
-					.catch(function (reason) {
-						$log.error('Use session experienced');
-
-						return $q.reject(reason);
-					});
-			}
-
-			function logout(user) {
-				return $http.post({
-						url: user.$url() + '/logout'
-					})
-					.then(function () {
-						$log.debug('Logged out user');
-					})
-					.catch(function (response) {
-						$log.error('Could not logout the user', response);
-
-						return $q.reject(response);
-					});
-			}
-
-			function resetPasswordInit(user) {
-				return $http.post({
-						url: user.$url() + '/password/reset/init',
-						data: {
-							email: user.email
-						}
-					})
-					.then(function () {
-						$log.debug('Sent email with password');
-					})
-					.catch(function (response) {
-						$log.error('Error sending email with password reset', response);
-
-						return $q.reject(response);
-					});
-			}
-
-			function resetPasswordFinish(user) {
-				return $http.post({
-						url: user.$url() + '/password/reset/finish',
-						data: {
-							token: user.token,
-							newPassword: user.password
-						}
-					})
-					.then(function () {
-						$log.debug('Changed user password');
-					})
-					.catch(function (response) {
-						$log.error('Error changing user password', response);
-
-						return $q.reject(response);
-					});
-			}
-
-			return api;
+		api.login = function (loginData) {
+			return User
+				.$build(loginData)
+				.$login()
+				.$asPromise()
+				.then(function (user) {
+					$log.debug('Successfully logged user in');
+					return user;
+				})
+				.catch(function (reason) {
+					$log.error('Could not login the user');
+					return $q.reject(reason);
+				});
 		};
 
-		module.service('UserAPI', UserAPI);
-	}
-);
+		api.register = function (user) {
+			return user
+				.$register()
+				.$asPromise()
+				.then(function (user) {
+					$log.debug('Successfully registered new user');
+					return user;
+				})
+				.catch(function (reason) {
+					$log.error('Error registering new user');
+					return $q.reject(reason);
+				});
+		};
+
+		api.authInfo = function authInfo() {
+			return User
+				.$build()
+				.$authInfo()
+				.$asPromise()
+				.then(function (user) {
+					$log.debug('Successfully verified user session');
+					return user;
+				})
+				.catch(function (reason) {
+					$log.error('Use session experienced');
+					return $q.reject(reason);
+				});
+		};
+
+		api.logout = function (user) {
+			return user
+				.$logout()
+				.$asPromise()
+				.then(function () {
+					$log.debug('Logged out user');
+				})
+				.catch(function (response) {
+					$log.error('Could not logout the user', response);
+					return $q.reject(response);
+				});
+		};
+
+		api.resetPasswordInit = function (user) {
+			return user
+				.$passwordResetInit()
+				.$asPromise()
+				.then(function () {
+					$log.debug('Sent email with password');
+				})
+				.catch(function (response) {
+					$log.error('Error sending email with password reset', response);
+					return $q.reject(response);
+				});
+		};
+
+		api.resetPassword = function (user) {
+			return user
+				.$passwordReset()
+				.$asPromise()
+				.then(function () {
+					$log.debug('Changed user password');
+				})
+				.catch(function (response) {
+					$log.error('Error changing user password', response);
+					return $q.reject(response);
+				});
+		};
+
+		return api;
+	};
+
+	module.service('UserAPI', UserAPI);
+});
 
 
