6919b94c33b0ff01c768b2a6a5d5a68ba957aa51
Przemysław Czekaj <pczekaj@neoteric.eu>
2016-06-01 14:14:03 +0200

    feat(exceptionHandler): handle exception and pass to stacktrace exception handler

---
+++ src/seed/helpers/_exceptions/exceptionHandler.js
@@ -0,0 +1,16 @@
+define(['seed/helpers/module'], function (module) {
+    'use strict';
+    
+    module.factory('$exceptionHandler', function($window) {
+        return function(exception, cause) {
+            if(cause) {
+                exception.message += ' (caused by "' + cause + '")';
+            }
+            exception.requireType = 'angular';
+            exception.requireModules = [cause];
+            if(typeof $window.handleStackTrace === 'function') {
+                $window.handleStackTrace(exception);
+            }
+        };
+    });
+});
